c2a4d926657818375b23e62c523dfd076706644d
Predeactor <pro.julien.mauroy@gmail.com>
2023-02-28 19:50:45 +0100

    feat: remove key pass in RemoteBan

--- remoteban/remoteban.py
+++ remoteban/remoteban.py
@@ -1,18 +1,14 @@
-import asyncio
-import base64
 from datetime import datetime
 from typing import Dict, List, Literal, Optional, TypedDict, Union
 
 import discord
-from discord import embeds
 from discord.errors import HTTPException
 from redbot.core import Config, commands
 from redbot.core.bot import Red
 from redbot.core.modlog import create_case
-from redbot.core.utils.chat_formatting import bold, inline, warning
+from redbot.core.utils.chat_formatting import bold, warning
 from redbot.core.utils.menus import DEFAULT_CONTROLS, menu
 from redbot.core.utils.mod import get_audit_reason
-from redbot.core.utils.predicates import MessagePredicate
 
 from .utils import allowed_to_ban
 
@@ -83,12 +79,11 @@ class UserCase:
                     for guild in self.fails.items()
                 ]
             )
-        embed = discord.Embed(
+        return discord.Embed(
             color=discord.Colour.dark_red(),
             title=f"{'Ban' if self.action == 'ban' else 'Unban'} Result For {str(self.user)}",
             description=description,
         )
-        return embed
 
 
 class RemoteBan(commands.Cog):
@@ -127,43 +122,8 @@ class RemoteBan(commands.Cog):
     def check_ban_permission_in_guild(self, guild: discord.Guild) -> Optional[bool]:
         return guild.me.guild_permissions.ban_members
 
-    async def start_interactive_questionning(self, ctx: commands.Context) -> bool:
-        await ctx.author.send(
-            "Before we allow you to add external users from being able, we need to tell you about consequences.\n"
-            "The good side of letting others users using `rban` is to do the same thing as you (Apart from setting up the cog), the bad side is that you have to trust the persons you will add.\n"
-            "Respecting sudo's warning, we boils down to these three things:\n\n"
-            "#1) Respect the privacy of others.\n"
-            "#2) Think before you type.\n"
-            "#3) With great power comes great responsibility.\n\n"
-            'We want you to understand that adding external users can be a problem, and you do not want bad things to happen. We made the cog as "legit" as possible to try to make things as non-destructive as possible while still keeping the main goal.\n'
-            f"Before adding any external users, {bold('you need to enter a key pass')} to be sure you have understood the risks.\n\n"
-            f"{bold('To obtain the key pass, please contact:')} {inline('Capt. Pred#0495')}."
-        )
-        await ctx.author.send(
-            bold("ENTER KEY PASS - YOU HAVE 5 MINUTES BEFORE YOU HAVE TO REEXECUTE THE COMMAND.")
-        )
-        check_ctx = MessagePredicate.same_context(channel=ctx.author.dm_channel, user=ctx.author)
-        try:
-            message = await ctx.bot.wait_for("message", check=check_ctx, timeout=300)
-        except asyncio.TimeoutError:
-            await ctx.author.send(bold("NO KEY PASS RECEIVED - REEXECUTE THE COMMAND."))
-            return False
-        if message.content == base64.b64decode(
-            b"SSBoZXJlYnkgYWxsb3cgdG8gYWRkIGV4dGVybmFsIHVzZXJzIHRvIHRoZSBjb2csIFJlbW90ZUJhbiwgYW5kIGFncmVlIHRvIGJlaW5nIHVuYWJsZSB0byBibGFtZSB0aGUgY3JlYXRvciBvZiB0aGUgY29nIFJlbW90ZUJhbiBmb3IgZGFtYWdlIGNhdXNlZCBieSB0aGUgY29nIHRvIERpc2NvcmQgc2VydmVycy4="
-        ).decode("utf-8"):
-            self.__has_accepted_conditions = True
-            await ctx.author.send(
-                bold(
-                    "KEY PASS ACCEPTED - YOU CAN NOW USE EXTERNAL USERS MANAGER COMMANDS AS YOU LIKE UNTIL BOT RESTART."
-                )
-            )
-            return True
-        else:
-            await ctx.author.send(bold("KEY PASS INVALID"))
-            return False
-
     async def add_server(self, ctx_author: discord.User, guild: Union[int, discord.Guild]):
-        if not isinstance(guild, discord.Guild):
+        if isinstance(guild, int):
             guild = self.bot.get_guild(guild)
         if not guild:
             raise commands.UserFeedbackCheckFailure(
@@ -175,11 +135,13 @@ class RemoteBan(commands.Cog):
             raise commands.UserFeedbackCheckFailure(
                 "I do not have the necessary permissions to ban users in this guild."
             )
-        if guild.owner != ctx_author:
-            if not guild.get_member(ctx_author.id).guild_permissions.administrator:
-                raise commands.UserFeedbackCheckFailure(
-                    "You are not owning this guild or you are not an administrator. I cannot let you do that."
-                )
+        if (
+            guild.owner != ctx_author
+            and not guild.get_member(ctx_author.id).guild_permissions.administrator
+        ):
+            raise commands.UserFeedbackCheckFailure(
+                "You are not owning this guild or you are not an administrator. I cannot let you do that."
+            )
         async with self.config.servers() as guilds:
             if guild.id in guilds:
                 raise commands.UserFeedbackCheckFailure("This guild is already registered.")
@@ -201,10 +163,7 @@ class RemoteBan(commands.Cog):
         return True
 
     async def remove_server(self, guild: Union[int, discord.Guild]):
-        if isinstance(guild, discord.Guild):
-            guild_id = guild.id
-        else:
-            guild_id = guild
+        guild_id = guild.id if isinstance(guild, discord.Guild) else guild
         async with self.config.servers() as guilds:
             if guild not in guilds:
                 raise commands.UserFeedbackCheckFailure("This guild is not registered.")
@@ -271,22 +230,23 @@ class RemoteBan(commands.Cog):
         guilds_removed: Optional[List[str]],
         action: Literal["unban", "ban"],
     ) -> List[discord.Embed]:
-        embeds = []
         wording = "unban" if action == "unban" else "ban"
-        total_fails = sum([len(user.fails) for user in users_cases])
-        total_success = sum([len(user.guilds_banned_or_unbanned) for user in users_cases])
+        total_fails = sum(len(user.fails) for user in users_cases)
+        total_success = sum(len(user.guilds_banned_or_unbanned) for user in users_cases)
         first_embed = discord.Embed(
             color=discord.Color.dark_orange(),
             title="Summary",
             description=(
-                "A total of {users_count} user(s) have been processed.\n\n"
-                "A total of {total_success} {wording}, for a total of {total_fails} failure(s).\n"
-                "A more in depth result is available in this menu."
-            ).format(
-                users_count=bold(str(len(fetched_users["users"]))),
-                total_success=bold(str(total_success)),
-                wording=bold(str(wording)),
-                total_fails=bold(str(total_fails)),
+                (
+                    "A total of {users_count} user(s) have been processed.\n\n"
+                    "A total of {total_success} {wording}, for a total of {total_fails} failure(s).\n"
+                    "A more in depth result is available in this menu."
+                ).format(
+                    users_count=bold(str(len(fetched_users["users"]))),
+                    total_success=bold(str(total_success)),
+                    wording=bold(wording),
+                    total_fails=bold(str(total_fails)),
+                )
             ),
         )
         if guilds_removed:
@@ -302,9 +262,8 @@ class RemoteBan(commands.Cog):
                 name=tick("All guilds have processed bans"),
                 value="No errors related to guild's permissions occured when banning users in guilds.",
             )
-        embeds.append(first_embed)
-        for user in users_cases:
-            embeds.append(user.to_embed())
+        embeds = [first_embed]
+        embeds.extend(user.to_embed() for user in users_cases)
         return embeds
 
     async def unban_users(
@@ -460,7 +419,7 @@ class RemoteBan(commands.Cog):
         """
         if state is None:
             return await ctx.send(
-                "Create a modlog entry on ban: " + yes_or_no(await self.config.send_modlog())
+                f"Create a modlog entry on ban: {yes_or_no(await self.config.send_modlog())}"
             )
         await self.config.send_modlog.set(state)
         await ctx.tick()
@@ -482,24 +441,18 @@ class RemoteBan(commands.Cog):
         """
         Add an external user being able to run `rban` commands.
         """
-        if not self.__has_accepted_conditions:
-            if not await self.start_interactive_questionning(ctx):
-                return
         if await self.add_user(user_id):
             return await ctx.send(
                 f"{ctx.author.mention} User ID added; Remember, With great power comes great responsibility."
             )
         else:
-            await ctx.send(f"This user has already been added.")
+            await ctx.send("This user has already been added.")
 
     @user_manager.command(name="list", aliases=["ls"])
     async def list_external_users_in_remoteban(self, ctx: commands.Context):
         """
         List all external users.
         """
-        if not self.__has_accepted_conditions:
-            if not await self.start_interactive_questionning(ctx):
-                return
         users = await self.config.allowed_users()
         msg_user = ""
         for user in users:
@@ -518,13 +471,10 @@ class RemoteBan(commands.Cog):
         """
         Remove an external user from being able to run `rban` commands.
         """
-        if not self.__has_accepted_conditions:
-            if not await self.start_interactive_questionning(ctx):
-                return
         if await self.remove_user(user_id):
             return await ctx.send(f"{ctx.author.mention} User ID removed.")
         else:
-            await ctx.send(f"This user is not allowed already.")
+            await ctx.send("This user is not allowed already.")
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild: discord.Guild):
